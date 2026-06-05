76ca615b623829c2b4ce1f4c963e2e6f9833cc58
Irai C Bocco <ircastro333@gmail.com>
2026-06-04 21:31:06 -0300

    commit inicial

---
+++ cypress/e2e/1-getting-started/homePlayground.cy.js
@@ -0,0 +1,14 @@
+// 
+describe ('Home de la pagina, validamos links', () => {
+it ('LoginOK', () => {
+cy.visit('https://cypress-playground.vercel.app/forms')
+cy.get('[data-testid="bp-name"]').type('Irai')
+})
+})
+describe ('completar datos', () => {
+it ('LoginOK', () => {
+cy.visit('https://cypress-playground.vercel.app/forms')
+cy.get('[data-testid="bp-name"]').type('Irai')
+
+})
+})
